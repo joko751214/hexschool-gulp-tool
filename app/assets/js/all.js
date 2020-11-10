f27351f67d6639a85536e48b2cce726d6325f066
joko751214 <47811731+joko751214@users.noreply.github.com>
2020-11-10 15:00:00 +0800

    finish week4

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,7 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
-});
+// $(document).ready(function() {
+//   $('.product-menu-list').on('click', function(e) {
+//     e.preventDefault();
+//     console.log(this);
+//     $('.product-menu-list a').toggleClass('active');
+//   });
+// });
